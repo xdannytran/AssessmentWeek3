fe0a48df2f64f425df3fc8bd081907e7f3f0ab9b
Danny Tran <xdannytran@hotmail.com>
2022-03-18 11:10:39 -0500

    finished

---
+++ profile.js
@@ -0,0 +1,13 @@
+<body>
+<p id="demo"></p>
+<script>
+
+document.getElementById("color").addEventListener("click");
+document.getElementById("place").addEventListener("click");
+document.getElementById("ritual").addEventListener("click");
+
+
+
+</script>
+<script src="/profile.hmtl" type="text/javascript"></script>
+</body>
